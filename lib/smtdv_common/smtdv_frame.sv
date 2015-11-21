67e5a32b04c6538b525d7f7acdb68dff1d8ceeac
Sean Chen <schen@twlogin04.stec-inc.ad>
2015-11-21 23:54:52 +0800

    add loopup table

---
+++ lib/smtdv_common/smtdv_frame.sv
@@ -0,0 +1,9 @@
+
+// DATA_WIDTH
+// [0:10]
+// field name, row range(0, 0), column range(0, 10) MSB/LSB
+// ex:
+// frame.init(5, 32, MSB)
+// frame.field("header", (0,0), (0,10))
+// frame.finalize();
+// frame.convert(); / frame.populate
---
+++ lib/smtdv_common/smtdv_lookup_table.sv
@@ -0,0 +1,6 @@
+// define global/local lookup table to turn on/off each smtdv module
+// ex:
+//  | path | CPU |  DMA | DDR
+// ===========================
+//    0    |  0 |    1  | 2
+//    1    |  x |    0  | 1
