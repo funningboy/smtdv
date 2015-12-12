4fcba45fc244c8787a1c5d5a6a7ce6b2d90793a6
Sean Chen <schen@twlogin04.stec-inc.ad>
2015-12-12 23:08:43 +0800

    test2

--- lib/smtdv_common/smtdv_vif.sv
+++ lib/smtdv_common/smtdv_vif.sv
@@ -4,11 +4,19 @@
 `timescale 1ns/10ps
 import uvm_pkg::*;
 `include "uvm_macros.svh"
+`include "smtdv_macros.svh"
 
 interface smtdv_if (
   input clk, // hclk
   input resetn // hresetn
   );
+
+//  bit has_force = `TRUE;
+//  bit has_checks = `TRUE;
+//  bit has_coverage = `TRUE;
+//  bit has_performance = `TRUE;
+
+
 endinterface: smtdv_if
 
 `endif
