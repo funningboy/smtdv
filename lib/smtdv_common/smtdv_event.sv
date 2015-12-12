5551cad2607cbed754936d08f88f2e2fdeeb5241
Sean Chen <schen@twlogin04.stec-inc.ad>
2015-12-12 23:23:43 +0800

    test6

--- lib/smtdv_common/smtdv_event.sv
+++ lib/smtdv_common/smtdv_event.sv
@@ -2,28 +2,94 @@
 `ifndef __SMTDV_EVENT_SV__
 `define __SMTDV_EVENT_SV__
 
-// *********************************************************************
-class smtdv_run_thread extends uvm_object;
+class smtdv_event extends uvm_object;
 
-  smtdv_component cmp;
-  bit [0:0] has_finalize = 0;
-  // status run, idle, dead...
-
-  `uvm_object_param_utils_begin(smtdv_run_thread)
-    `uvm_field_int(has_finalize, UVM_DEFAULT)
+  `uvm_object_param_utils_begin(smtdv_event)
   `uvm_object_utils_end
 
-  function new(string name = "smtdv_run_thread", smtdv_component cmp=null);
+  function new(string name = "smtdv_event");
     super.new(name);
-    cmp = cmp;
   endfunction
 
-  virtual task run();
-    `uvm_info(get_full_name(),
-      $sformatf("Starting run thread ..."),
-      UVM_HIGH)
-  endtask
+endclass
+
+// smtdv register updated cfg event while after setting reg cfg
+//
+class smtdv_cfg_evt #(
+  ADDR_WIDTH = 14,
+  DATA_WIDTH = 32
+) extends
+  smtdv_event;
+
+  typedef struct {
+    bit [0:0] match;
+    bit [ADDR_WIDTH-1:0] addr;
+    bit [DATA_WIDTH-1:0] mask;
+    bit [DATA_WIDTH-1:0] data;
+    bit require;
+    string depent;
+    bit visit;
+  } rule;
+
+  // virtual TLM notify item get
+  smtdv_sequence_item#(ADDR_WIDTH, DATA_WIDTH) item;
+  // virtual updated cfg
+  smtdv_cfg cfg;
+
+  string rulenm;
+  rule maps[string];
+
+  `uvm_object_param_utils_begin(smtdv_cfg_evt#(ADDR_WIDTH, DATA_WIDTH))
+  `uvm_object_utils_end
+
+  function new(string name = "smtdv_event", smtdv_cfg cfg=null, smtdv_sequence_item#(ADDR_WIDTH, DATA_WIDTH) item=null);
+    super.new(name);
+    cfg = cfg;
+    item = item;
+  endfunction
+
+  virtual function void pre_do();
+  endfunction
+
+  virtual function void run();
+    while (maps.next(rulenm)) begin
+      foreach(item.addrs[i]) begin
+        if (item.addrs[i] == maps[rulenm].addr) begin
+          if (maps[rulenm].match) begin
+            if (item.unpack_data(i) & maps[rulenm].mask == maps[rulenm].data) begin
+              maps[rulenm].visit = TRUE;
+            end
+          end
+          else begin
+            maps[rulenm].data = item.unpack_data(i) & maps[rulenm].mask;
+            maps[rulenm].visit = TRUE;
+          end
+        end
+      end
+    end
+  endfunction
+
+  virtual function bit ready_to_update();
+    while (maps.next(rulenm)) begin
+      if (maps[rulenm].visit == FALSE && maps[rulenm].require == TRUE) begin
+        return FALSE;
+      end
+    end
+    return TRUE;
+  endfunction
+
+  virtual function void ready_to_clear();
+    while (maps.next(rulenm)) begin
+      maps[rulenm].visit = FALSE;
+    end
+  endfunction
+
+  virtual function void update_cfg();
+  endfunction
 
 endclass
+
+
+
 
 `endif // __SMTDV_EVENT_SV__
--- lib/smtdv_common/smtdv_sequence_item.sv
+++ lib/smtdv_common/smtdv_sequence_item.sv
@@ -2,25 +2,52 @@
 `ifndef __SMTDV_SEQUENCE_ITEM_SV__
 `define __SMTDV_SEQUENCE_ITEM_SV__
 
-class smtdv_sequence_item extends uvm_sequence_item;
+class smtdv_base_item
+  extends
+  uvm_sequence_item;
 
   bit                 debug = TRUE;
-  rand bit [31:0]     addr;
-  rand bit [31:0]     addrs[$];
-  rand bit [3:0][7:0] data_beat[$];
-  rand bit [3:0][0:0] byten_beat[$];
-
-  bit                 complete = 0;
-  bit                 addr_complete = 0;
-  bit                 data_complete = 0;
-  int                 id = 0;
   int                 uuid = 0;
+
+  smtdv_base_item next = null;
+  smtdv_base_item pre = null;
+  smtdv_base_item parent = null;
+
+  `uvm_object_param_utils_begin(smtdv_base_item)
+    if (debug) begin
+      `uvm_field_int(uuid, UVM_ALL_ON)
+      `uvm_field_object(pre, UVM_DEFAULT)
+      `uvm_field_object(next, UVM_DEFAULT)
+    end
+  `uvm_object_utils_end
+
+  function new(string name = "smtdv_base_item");
+    super.new(name);
+  endfunction
+
+endclass
+
+class smtdv_sequence_item #(
+    ADDR_WIDTH = 14,
+    DATA_WIDTH = 32
+  ) extends
+  smtdv_base_item;
+
+  bit                 debug = TRUE;
+  rand bit [ADDR_WIDTH-1:0]     addr;
+  rand bit [ADDR_WIDTH-1:0]     addrs[$];
+  rand bit [(DATA_WIDTH>>3)-1:0][7:0] data_beat[$];
+  rand bit [(DATA_WIDTH>>3)-1:0][0:0] byten_beat[$];
+  rand int            offset = DATA_WIDTH>>3;
+
+  bit                 success = FALSE;
+  bit                 mem_complete = FALSE;
+  bit                 fifo_complete = FALSE;
+  bit                 addr_complete = FALSE;
+  bit                 data_complete = FALSE;
+  int                 id = 0;
   int 	              rsp = 0;
   int                 bst_len = 0;
-
-  smtdv_sequence_item next = null;
-  smtdv_sequence_item pre = null;
-  smtdv_sequence_item parent = null;
 
   int                 addr_idx = 0;
   int                 data_idx = 0;
@@ -39,28 +66,29 @@ class smtdv_sequence_item extends uvm_sequence_item;
     life_time inside {[10:20]};
   }
 
-  `uvm_object_param_utils_begin(smtdv_sequence_item)
+  `uvm_object_param_utils_begin(smtdv_sequence_item#(ADDR_WIDTH, DATA_WIDTH))
     `uvm_field_int(id, UVM_ALL_ON)
     // virtual field should been imp at top level
-    //`uvm_field_int(addr, UVM_ALL_ON)
-    //`uvm_field_int(bst_len, UVM_ALL_ON)
-    //`uvm_field_int(rsp, UVM_ALL_ON)
-    //`uvm_field_queue_int(addrs, UVM_ALL_ON)
-    //`uvm_field_queue_int(data_beat, UVM_ALL_ON)
-    //`uvm_field_queue_int(byten_beat, UVM_ALL_ON)
-    `uvm_field_int(complete, UVM_ALL_ON)
-    `uvm_field_int(addr_complete, UVM_ALL_ON)
-    `uvm_field_int(data_complete, UVM_ALL_ON)
-    `uvm_field_int(addr_idx, UVM_ALL_ON)
-    `uvm_field_int(data_idx, UVM_ALL_ON)
-    `uvm_field_enum(mod_type_t, mod_t, UVM_ALL_ON)
-    `uvm_field_enum(trs_type_t, trs_t, UVM_ALL_ON)
-    `uvm_field_enum(run_type_t, run_t, UVM_ALL_ON)
+    `uvm_field_int(addr, UVM_ALL_ON)
+    `uvm_field_int(bst_len, UVM_ALL_ON)
+    `uvm_field_int(rsp, UVM_ALL_ON)
+    `uvm_field_queue_int(addrs, UVM_ALL_ON)
+    `uvm_field_queue_int(data_beat, UVM_ALL_ON)
+    `uvm_field_queue_int(byten_beat, UVM_ALL_ON)
     // hiden info
     if (debug) begin
-      `uvm_field_int(uuid, UVM_ALL_ON)
-      `uvm_field_object(pre, UVM_DEFAULT)
-      `uvm_field_object(next, UVM_DEFAULT)
+      `uvm_field_int(offset, UVM_ALL_ON)
+      `uvm_field_int(success, UVM_ALL_ON)
+      `uvm_field_int(mem_complete, UVM_ALL_ON)
+      `uvm_field_int(fifo_complete, UVM_ALL_ON)
+      `uvm_field_int(addr_complete, UVM_ALL_ON)
+      `uvm_field_int(data_complete, UVM_ALL_ON)
+      `uvm_field_int(addr_idx, UVM_ALL_ON)
+      `uvm_field_int(data_idx, UVM_ALL_ON)
+      `uvm_field_enum(mod_type_t, mod_t, UVM_ALL_ON)
+      `uvm_field_enum(trs_type_t, trs_t, UVM_ALL_ON)
+      `uvm_field_enum(run_type_t, run_t, UVM_ALL_ON)
+
       `uvm_field_int(bg_cyc, UVM_ALL_ON)
       `uvm_field_int(ed_cyc, UVM_ALL_ON)
       `uvm_field_int(bg_time, UVM_ALL_ON)
@@ -72,31 +100,47 @@ class smtdv_sequence_item extends uvm_sequence_item;
     super.new(name);
   endfunction
 
-  function bit compare(smtdv_sequence_item cmp);
+  virtual function void pack_data(int idx=0, bit [DATA_WIDTH-1:0] idata=0);
+    int n = (DATA_WIDTH>>3);
+    for (int i=0; i<n; i+=1) begin
+      data_beat[idx][i] = idata[i*8+:8];
+    end
+  endfunction
+
+  virtual function bit[DATA_WIDTH-1:0] unpack_data(int idx=0);
+    bit [DATA_WIDTH-1:0] odata;
+    int n = (DATA_WIDTH>>3);
+    for (int i=0; i<n; i+=1) begin
+      odata[i*8+:8] = data_beat[idx][i];
+    end
+    return odata;
+  endfunction
+
+  virtual function bit compare(smtdv_sequence_item#(ADDR_WIDTH, DATA_WIDTH) cmp);
     // addr cmp
-    if (this.addrs.size() != cmp.addrs.size() ) begin
+    if (addrs.size() != cmp.addrs.size() ) begin
       return FALSE;
     end
-    foreach(this.addrs[i]) begin
-      if (this.addrs[i] != cmp.addrs[i]) begin
+    foreach(addrs[i]) begin
+      if (addrs[i] != cmp.addrs[i]) begin
         return FALSE;
       end
     end
     // data_beat cmp
-    if (this.data_beat.size() != cmp.data_beat.size() ) begin
+    if (data_beat.size() != cmp.data_beat.size() ) begin
         return FALSE;
     end
-    foreach(this.data_beat[i]) begin
-      if (this.data_beat[i] != cmp.data_beat[i]) begin
+    foreach(data_beat[i]) begin
+      if (data_beat[i] != cmp.data_beat[i]) begin
         return FALSE;
       end
     end
     // byten cmp
-    if (this.byten_beat.size() != cmp.byten_beat.size() ) begin
+    if (byten_beat.size() != cmp.byten_beat.size() ) begin
       return FALSE;
     end
-    foreach(this.byten_beat[i]) begin
-      if (this.byten_beat[i] != cmp.byten_beat[i]) begin
+    foreach(byten_beat[i]) begin
+      if (byten_beat[i] != cmp.byten_beat[i]) begin
         return FALSE;
       end
     end
