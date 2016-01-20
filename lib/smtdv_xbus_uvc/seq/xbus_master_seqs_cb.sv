138db0713eb9abd3c872f7e3022bb7bd0e094ca5
Sean Chen <schen@twlogin04.stec-inc.ad>
2016-01-20 17:46:34 +0800

    rm xbus_uvc

--- lib/smtdv_xbus_uvc/seq/xbus_master_seqs_cb.sv
+++
@@ -1,7 +0,0 @@
-
-// adaptor: extract from sqlite3 db or TLM1 systemc analysis port
-`ifndef __XBUS_MASTER_SEQS_CB_SV__
-`define __XBUS_MASTER_SEQS_CB_SV__
-
-
-`endif // __XBUS_MASTER_SEQS_CB_SV__
--- lib/smtdv_xbus_uvc/seq/xbus_slave_seqs_cb.sv
+++
@@ -1,8 +0,0 @@
-
-// adaptor: extract from sqlite3 db or TLM1 systemc analysis port
-
-`ifndef __XBUS_SLAVE_SEQS_CB_SV__
-`define __XBUS_SLAVE_SEQS_CB_SV__
-
-`endif // end of __XBUS_SLAVE_SEQS_CB_SV__
-
